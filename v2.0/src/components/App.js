f73307b45f628522cfe2de4ad0cdc540b735fce0
taleore <taleore@gmail.com>
2019-09-04 21:01:33 +0200

    add folder

---
+++ v2.0/src/components/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../stylesheets/App.scss";
+import header from "./Header";
+
+function App() {
+  return <div className="App">{header}</div>;
+}
+
+export default App;
