faaad26605e7670f00d81cc2db1b5c5abbc2ba07
laila25 <laila2519@gmail.com>
2019-09-10 20:05:09 +0200

    finish design

--- v2.0/src/index.js
+++ v2.0/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./stylesheets/index.scss";
+import "./stylesheets/reset.scss";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
