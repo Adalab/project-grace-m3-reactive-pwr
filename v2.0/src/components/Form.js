28d89a89ce49814cf3448d3613de5e1b47264ff0
laila25 <laila2519@gmail.com>
2019-09-17 09:14:39 +0200

    finish icon update

--- v2.0/src/components/Form.js
+++ v2.0/src/components/Form.js
@@ -25,15 +25,16 @@ class Form extends React.Component {
 
   render() {
     const actionToForm = this.props.actionToForm;
-    const actionToName = this.props.actionToName;
-    const actionToJob = this.props.actionToJob;
     return (
       <section className={`data js-collapse ${this.state.class}`}>
         <div className="data__title">
           <h2 className="data__title__text home--title">
             <i className="data__title__text__icon far fa-keyboard"></i>Rellena
           </h2>
-          <button className="data__title__btn js-btn-collapse" onClick={this.putHiddenClass}>
+          <button
+            className="data__title__btn js-btn-collapse"
+            onClick={this.putHiddenClass}
+          >
             <i className="fa fa-chevron-up"></i>
           </button>
         </div>
@@ -42,11 +43,23 @@ class Form extends React.Component {
             <label htmlFor="name" className="data__form-label">
               Nombre completo
             </label>
-            <input type="text" name="name" className="data__form-item js-form_name" placeholder="Ej: Sally Jill" onChange={actionToForm} />
+            <input
+              type="text"
+              name="name"
+              className="data__form-item js-form_name"
+              placeholder="Ej: Sally Jill"
+              onChange={actionToForm}
+            />
             <label htmlFor="job" className="data__form-label">
               Puesto
             </label>
-            <input type="text" name="job" className="data__form-item js-form__job" placeholder="Ej: Front-end unicorn" onChange={actionToForm} />
+            <input
+              type="text"
+              name="job"
+              className="data__form-item js-form__job"
+              placeholder="Ej: Front-end unicorn"
+              onChange={actionToForm}
+            />
             <label htmlFor="user_image" className="data__form__image-label">
               Imagen de perfil
             </label>
@@ -60,20 +73,44 @@ class Form extends React.Component {
             <label htmlFor="email" className="data__form-label">
               Email
             </label>
-            <input type="email" name="email" className="data__form-item js-link_email" placeholder="Ej: adalab@gmail.com" onChange={actionToForm} />
+            <input
+              type="email"
+              name="email"
+              className="data__form-item js-link_email"
+              placeholder="Ej: adalab@gmail.com"
+              onChange={actionToForm}
+            />
             <label htmlFor="job" className="data__form-label">
               Teléfono
             </label>
-            <input type="tel" name="phone" className="data__form-item js-link_phone" placeholder="Ej: 632-54-12-36" onChange={actionToForm} />
+            <input
+              type="tel"
+              name="phone"
+              className="data__form-item js-link_phone"
+              placeholder="Ej: 632-54-12-36"
+              onChange={actionToForm}
+            />
             <label htmlFor="linkedin" className="data__form-label">
               LinkedIn
             </label>
-            <input type="text" name="linkedin" className="data__form-item js-link_linkedin" placeholder="Ej: sally.hill" onChange={actionToForm} />
+            <input
+              type="text"
+              name="linkedin"
+              className="data__form-item js-link_linkedin"
+              placeholder="Ej: sally.hill"
+              onChange={actionToForm}
+            />
             <label htmlFor="github" className="data__form-label">
               GitHub
             </label>
             <span className="data__form-before">@</span>
-            <input type="text" name="github" className="data__form-item js-link_github" placeholder="Ej: @sallyHill" onChange={actionToForm} />
+            <input
+              type="text"
+              name="github"
+              className="data__form-item js-link_github"
+              placeholder="Ej: sallyHill"
+              onChange={actionToForm}
+            />
           </div>
         </div>
       </section>
--- v2.0/src/components/ViewerIcon.js
+++ v2.0/src/components/ViewerIcon.js
@@ -1,38 +1,73 @@
 import React from "react";
 
 class ViewerIcon extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      email: null
-    };
-  }
-
   render() {
     let email = this.props.data.email;
-
-    console.log(email);
+    let linkedin = this.props.data.linkedin;
+    let github = this.props.data.github;
+    let phone = this.props.data.phone;
 
     return (
       <ul className="visualization__user__icon-list">
         <li className="icon-wrap border--color js-phone">
-          <i className="icon fa fa-mobile-alt"></i>
+          {phone === "" ? (
+            <i className="icon fa fa-mobile-alt"> </i>
+          ) : (
+            <a href={`tel:${phone}`} target="_blank" rel="noopener noreferrer">
+              <i
+                style={{ opacity: "1" }}
+                className="icon fa fa-mobile-alt js-envelope"
+              ></i>
+            </a>
+          )}
         </li>
         <li className="icon-wrap border--color js-email">
           {email === "" ? (
             <i className="icon fa fa-envelope"> </i>
           ) : (
-            <a href={`mailto:  ${email}`} target="_blank">
-              <i className="icon fa fa-envelope js-envelope"></i>
+            <a
+              href={`mailto:${email}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i
+                style={{ opacity: "1" }}
+                className="icon fa fa-envelope js-envelope"
+              ></i>
             </a>
           )}
         </li>
         <li className="icon-wrap border--color js-linkedin">
-          <i className="icon fab fa-linkedin-in"></i>
+          {linkedin === "" ? (
+            <i className="icon fab fa-linkedin-in"> </i>
+          ) : (
+            <a
+              href={`https://www.linkedin.com/in/${linkedin}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i
+                style={{ opacity: "1" }}
+                className="icon fab fa-linkedin-in js-envelope"
+              ></i>
+            </a>
+          )}
         </li>
         <li className="icon-wrap border--color js-github">
-          <i className="icon fab fa-github-alt"></i>
+          {github === "" ? (
+            <i className="icon fab fa-github-alt"> </i>
+          ) : (
+            <a
+              href={`https://github.com/${github}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i
+                style={{ opacity: "1" }}
+                className="icon fab fa-github-alt js-envelope"
+              ></i>
+            </a>
+          )}
         </li>
       </ul>
     );
